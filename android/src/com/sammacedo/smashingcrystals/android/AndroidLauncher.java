722bf6f51b322902a79f7c4b398d374e35109ba8
Samuel Macedo <samuel-macedo-v@hotmail.com>
2015-10-04 23:36:18 -0700

    Google Play Services Removido

--- android/src/com/sammacedo/smashingcrystals/android/AndroidLauncher.java
+++ android/src/com/sammacedo/smashingcrystals/android/AndroidLauncher.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.sammacedo.smashingcrystals.Game;
-import com.google.android.gms.games.*;
 
 public class AndroidLauncher extends AndroidApplication
 {
